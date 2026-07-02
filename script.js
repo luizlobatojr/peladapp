841b3cfd7ab25d7e57de6df2f9ce684fdf422f2a
luizlobatojr <luizlobatojr@gmail.com>
2026-07-02 08:56:33 +0100

    v0.0.6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((reg) => {
         console.log("Service Worker registrado com sucesso!");
 
